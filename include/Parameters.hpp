1cfd00ccd676f211df94efa785866d87f85eac76
ilyuzaaaaa <ilyuza.yangirova@gmail.com>
2021-01-14 13:35:28 +0500

    1

---
+++ include/Parameters.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Илюза Янгирова on 12.01.2021.
+//
+
+
+#pragma once
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <iostream>
+#include <string>
+
+namespace po = boost::program_options;
+
+struct Parameters {
+    static int timeout;
+    static bool isInstallAdded;
+    static bool isPackAdded;
+    static std::string buildConfig;
+    static int prepareCommandLine(int argc, char **argv);
+};
---
+++ include/Process.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Илюза Янгирова on 12.01.2021.
+//
+
+#ifndef INCLUDE_PROCESS_HPP_
+#define INCLUDE_PROCESS_HPP_
+
+#include <boost/process.hpp>
+#include <chrono>
+#include <list>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "Builder.hpp"
+#include "Parameters.hpp"
+#include "ThreadData.hpp"
+
+class Process {
+public:
+    static void create(std::unique_ptr<ThreadData> &data);
+    static void startProcess(const std::list<std::string> &commandArg,
+                             std::unique_ptr<ThreadData> &data);
+};
+
+#endif  // INCLUDE_PROCESS_HPP_
